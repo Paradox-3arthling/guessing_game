05c97ac002563e1dde90f4564e0aa21fce3d0671
paradox-3arthling <floydqaranja@gmail.com>
2019-09-20 12:15:32 +0300

    Now standard to 1st snip of code in the #rust docs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
 
 fn main() {
-    println!("Welcome to the guessing game");
-    println!("Insert guess");
+    println!("Welcome to the guessing game :D");
+    println!("Enter your guess, press <q> to stop guessing.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("You guessed: {}", guess);
 }
