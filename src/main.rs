96691f98e29e299f32232e90d2aab35fdbedeba1
paradox-3arthling <floydqaranja@gmail.com>
2019-09-26 18:44:06 +0300

    I think it's now identical to the one in rust doc :) time for some more pazzaz!

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,29 @@ use rand::Rng;
 
 fn main() {
     println!("Welcome to the guessing game :D");
-    println!("To kill me, input a non-numiric value! .\\/_");
+    println!("To kill me, input <ctrl + c> .\\/_");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("the secret no. is {}", secret_number);
+    // println!("the secret no. is {}", secret_number);
     loop{
         println!("Enter your guess, sQuire!! .\\/.");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
-        let guess:u32 = guess.trim().parse()
-            .expect("please insert number!");
-        
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Only numbers FOOL! 00");
+                continue;
+            }
+        };        
 
         match guess.cmp(&secret_number){
             Ordering::Greater => println!("Too high!"),
             Ordering::Less => println!("Too low!"),
-            Ordering::Equal => println!("You guessed correct")
+            Ordering::Equal => {
+                println!("You guessed correct");
+                break;
+            }
          }
         }
 }
