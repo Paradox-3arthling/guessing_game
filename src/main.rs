f46b6bd320614166ed0e3e20315af53c927ee595
paradox-3arthling <floydqaranja@gmail.com>
2019-09-26 18:17:50 +0300

    cheeky sponse and insetion of the loop

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,22 @@ use rand::Rng;
 
 fn main() {
     println!("Welcome to the guessing game :D");
-    println!("Enter your guess, press <q> to stop guessing.");
+    println!("To kill me, input a non-numiric value! .\\/_");
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("the secret no. is {}", secret_number);
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess:u32 = guess.trim().parse()
-        .expect("please insert number!");
-    println!("You guessed: {}", guess);
+    loop{
+        println!("Enter your guess, sQuire!! .\\/.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess:u32 = guess.trim().parse()
+            .expect("please insert number!");
+        
 
-    match guess.cmp(&secret_number){
-        Ordering::Greater => println!("Too high!"),
-        Ordering::Less => println!("Too low!"),
-        Ordering::Equal => println!("You guessed correct")
-    }
+        match guess.cmp(&secret_number){
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Less => println!("Too low!"),
+            Ordering::Equal => println!("You guessed correct")
+         }
+        }
 }
