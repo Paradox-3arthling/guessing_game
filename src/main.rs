1d2532a8f10a8dcaecc85c1a72aa0e4b06845781
savage_floweed <FloydQaranja@gmail.com>
2019-09-26 14:21:57 +0300

    Added the latest  crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
+use rand::Rng;
+
 
 fn main() {
     println!("Welcome to the guessing game :D");
     println!("Enter your guess, press <q> to stop guessing.");
+    let secret_number = rand::thread_rn
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");    
     println!("You guessed: {}", guess);
 }
